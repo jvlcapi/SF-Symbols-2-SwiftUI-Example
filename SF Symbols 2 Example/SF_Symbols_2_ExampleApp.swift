5f6f4468a0bda684ae25e26a630ce7a8856b8d16
João Vitor Lopes Capi <joaovitorlopescapi@gmail.com>
2020-07-02 16:51:09 -0300

    Initial Commit

---
+++ SF Symbols 2 Example/SF_Symbols_2_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_Symbols_2_ExampleApp.swift
+//  SF Symbols 2 Example
+//
+//  Created by João Vitor Lopes Capi on 02/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SF_Symbols_2_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
